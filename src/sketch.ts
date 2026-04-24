9823a0fb4800343c485afebb78d1eb06c2b1852c
nbogie <neillbogie@googlemail.com>
2026-04-24 11:29:19 +0100

    import transparency() and register the addon it explicitly

--- src/sketch.ts
+++ src/sketch.ts
@@ -3,7 +3,8 @@
 // Load p5 in global mode: this registers all p5 APIs on window and the
 // `declare global {}` block in p5/global brings types into scope globally.
 import p5 from "p5/global";
-import "p5.transparency";
+import transparency from "p5.transparency";
+(p5 as any).registerAddon(transparency);
 
 // ----- sketch ----------------------------------------------------------------
 let locations: any;
